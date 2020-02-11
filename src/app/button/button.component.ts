2cac2c16e3f96e9a15299ce156aa4c537c0faa9a
Thiagoasd <thiago_teixeira4@yahoo.com.br>
2020-02-11 13:31:05 -0300

    Exercicios dia 5

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  EstaDisabilitado: boolean = true;
+  cor: string = "blue";
+
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(this.trocaCor, 1000);
   }
+
+
+  interruptor() {
+    this.EstaDisabilitado = !this.EstaDisabilitado;
+
+  }
+
+  trocaCor() {
+
+    if (this.cor === "blue") {
+      this.cor = "red";
+    } else {
+      this.cor = "blue";
+    }
+  }
+
+  trocaCorAuto() {
+
+    if (this.cor === "blue") {
+      this.cor = "red";
+    } else {
+      this.cor = "blue";
+    }
+
+    console.log("estou aqui")
+    setTimeout(this.trocaCorAuto, 1000);
+    console.log("passei o timeout");
+  }
+
+  exibirId(id: string) {
+    console.log(id);
+  }
+
 
 }
